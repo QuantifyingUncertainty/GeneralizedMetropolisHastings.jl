f5610fb7143ccbb6041ce03151ff76df84c6de43
Kris De Meyer <kris@aniku.co.uk>
2015-06-23 14:59:18 +0100

    Redo previous submit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,30 @@
 using GeneralizedMetropolisHastings
 using Base.Test
 
-tests =
-  ["odetest1",
-   "gaussiantest1",
-   "gaussiantest2"]
+unittests = [
+  "indicatorsampletest"]
 
-println("Running tests:")
+functionalitytests = [
+  "odetest1",
+  "gaussiantest1",
+  "gaussiantest2"]
 
-for t in tests
+println("==================")
+println("Running unittests:")
+println("==================")
+
+for t in unittests
   tfile = t*".jl"
   println("  * $(tfile) *")
   include(tfile)
 end
+
+println("===========================")
+println("Running functionalitytests:")
+println("===========================")
+
+# for t in functionalitytests
+#   tfile = t*".jl"
+#   println("  * $(tfile) *")
+#   include(tfile)
+# end
