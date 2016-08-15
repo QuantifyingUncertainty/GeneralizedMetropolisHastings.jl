34d25ea7ed76a829926d559f571d38ecab73208d
Kris De Meyer <KrisDM@users.noreply.github.com>
2016-08-15 23:15:15 +0200

    Delete testfile.jl

--- test/testfile.jl
+++
@@ -1,3 +0,0 @@
-testpdf1!(r::AbstractVector,d::Distribution,m::Matrix{Float64}) = (logpdf!(r,d,m) ; r)
-testpdf2!(r::AbstractVector,d::Distribution,m::Matrix{Float64}) = (for i=1:length(r) r[i] = logpdf(d,m[:,i]) end ; r)
-testpdf3!(r::AbstractVector,d::Distribution,v::Vector{Vector{Float64}}) = (for i=1:length(r) r[i] = logpdf(d,v[i]) end ; r)
